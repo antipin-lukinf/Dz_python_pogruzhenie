c58da67fdf0508cd98e4ac2875acc582b657ebd3
Drei4 <antipin-lukinf@mail.ru>
2023-06-19 21:36:46 +0300

    zd2.py complit_complit

--- Kl_rb4+Dz/zd2.py
+++ Kl_rb4+Dz/zd2.py
@@ -4,3 +4,21 @@
 ✔ Сформируйте список с уникальными кодами Unicode каждого
 символа введённой строки отсортированный по убыванию.
 """
+
+from operator import itemgetter
+
+string_str = "Напишите функцию, которая принимает строку текста"
+uni_list = list()
+
+
+def my_list(string_str):
+    mod_str = list(string_str)
+    for i in mod_str:
+        uni_list.append([ord(i)])
+
+    return uni_list.sort()
+
+
+my_list(string_str)
+
+print(uni_list)
