d151cd8fbca998e9d858e70cacff9b1b9cfa4a9f
Drei4 <antipin-lukinf@mail.ru>
2023-06-20 15:18:44 +0300

    Dz_zd_1.py complit

---
+++ Kl_rb4+Dz/Dz_zd_1.py
@@ -0,0 +1,20 @@
+# Напишите функцию для транспонирования матрицы
+
+matrix = [[1, 2, 3],
+          [1, 2, 3],
+          [1, 2, 3]]
+
+t_matrix = [[0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]]
+
+
+def transponir_matrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            t_matrix[j][i] = matrix[i][j]
+
+    return t_matrix
+
+
+print(transponir_matrix(matrix))
