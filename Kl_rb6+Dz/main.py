a56a981dbeaf65fff7e1425eabf90ccba1396e8d
Drei4 <antipin-lukinf@mail.ru>
2023-06-26 19:02:19 +0300

    zd2, zd3 complit

---
+++ Kl_rb6+Dz/main.py
@@ -0,0 +1,6 @@
+import zd_2
+import sys
+
+init_numbers = [i for i in sys.argv]
+
+zd_2.gues_f(int(init_numbers[3]), int(init_numbers[2]), int(init_numbers[1]))
--- Kl_rb6+Dz/zd_2.py
+++ Kl_rb6+Dz/zd_2.py
@@ -6,23 +6,40 @@
 Функция выводит подсказки “больше” и “меньше”.
 Если число угадано, возвращается истина, а если попытки исчерпаны - ложь.
 
+3.
+Улучшаем задачу 2.
+Добавьте возможность запуска функции “угадайки” из модуля в командной строке терминала.
+Строка должна принимать от 1 до 3 аргументов: параметры вызова функции.
+Для преобразования строковых аргументов командной строки в числовые параметры используйте генераторное выражение.
+
+
 """
 import random
 
-number_up = int(input('Введите верхнюю границу диапазона : '))
-number_down = int(input('Введите нижнюю границу диапазона : '))
-count = int(input('Введите количество попыток : '))
 
-def wtf(number_up, number_down, count):
-    numb_rand = random.randint(number_down, number_up)
+# number_up = int(input('Введите верхнюю границу диапазона : '))
+# number_down = int(input('Введите нижнюю границу диапазона : '))
+# count = int(input('Введите количество попыток : '))
 
-    for i in range(count):
-        my_number = int(input(f'Угадай число, которое я загадал, у тебя осталось {count - i} попыток :'))
-        if my_number < numb_rand:
-            print(f'мое число больше, у тебя осталось {count - i - 1} попыток')
-        elif my_number > numb_rand:
-            print(f'мое число меньше, у тебя осталось {count - i - 1} попыток')
-        elif my_number == numb_rand:
-            return True
+def gues_f(minimum, maximum, tries):
+    numb_rand = random.randint(minimum, maximum)
 
-print(wtf(number_up, number_down, count))
+    while tries:
+        my_number = int(input('Угадай число, которое я загадал :'))
+        if my_number == numb_rand:
+            print('Угадал')
+            break
+        else:
+            if my_number < numb_rand:
+                print('мое число больше')
+            else:
+                print('мое число меньше')
+
+        tries -= 1
+
+    else:
+        print(f'Вы не угадали, было загаданно число {numb_rand}')
+
+
+# if __name__ == "__main__":
+#     print(wtf(number_up, number_down, count))
